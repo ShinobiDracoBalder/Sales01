33a5711bc30797d0d1382a4ba9d59a16645482fb
Desarrollo hunter master <desarrollodracohuntermaster@gmail.com>
2018-10-31 10:21:15 -0600

    Changd API

---
+++ Sales01.API/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿namespace Sales01.API.Controllers
+{
+    using Sales01.Domain.Models;
+    using System.Linq;
+    using System.Web.Http;
+
+    
+    [RoutePrefix("api/Categories")]
+    public class CategoriesController : ApiController
+    {
+        private DataContext db = new DataContext();
+
+        [HttpGet]
+       // [Authorize]
+        public IQueryable<Category> GetCategories()
+        {
+            return db.Categories.OrderBy(c => c.Description);
+        }        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
