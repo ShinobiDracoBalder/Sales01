f5bb887769065e2db79b6ae2cd8f2b5545070a69
Desarrollo hunter master <desarrollodracohuntermaster@gmail.com>
2018-11-06 11:45:59 -0600

    Piker Category

--- Sales01.Common/Models/ProductRequest.cs
+++ Sales01.Common/Models/ProductRequest.cs
@@ -30,6 +30,10 @@
 
         public decimal Quantity { get; set; }
 
+        public int CategoryId { get; set; }
+
+        public int UserId { get; set; }
+
         public string ImageFullPath
         {
             get
