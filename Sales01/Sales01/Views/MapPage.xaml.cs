6cb2ab65612b6577ed643825a22c5d0ad508c9ee
Desarrollo hunter master <desarrollodracohuntermaster@gmail.com>
2018-11-09 10:54:44 -0600

    Changed Pins Maps Xamarin

--- Sales01/Sales01/Views/MapPage.xaml.cs
+++ Sales01/Sales01/Views/MapPage.xaml.cs
@@ -2,10 +2,15 @@
 {
     using Plugin.Geolocator;
     using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
     using Xamarin.Forms;
     using Xamarin.Forms.Maps;
     using Xamarin.Forms.Xaml;
-
+    using Services;
+    using Helpers;
+    using Common.Models;
 
     [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MapPage : ContentPage
@@ -37,8 +42,41 @@
             {
                 ex.ToString();
             }
+            var pins = await this.GetPins();
+            this.ShowPins(pins);
         }
-    
+        private void ShowPins(List<Pin> pins)
+        {
+            foreach (var pin in pins)
+            {
+                this.MyMap.Pins.Add(pin);
+            }
+        }
+
+        private async Task<List<Pin>> GetPins()
+        {
+            var pins = new List<Pin>();
+            var apiService = new ApiService();
+            var url = Application.Current.Resources["UrlAPI"].ToString();
+            var prefix = Application.Current.Resources["UrlPrefix"].ToString();
+            var controller = Application.Current.Resources["UrlProductsController"].ToString();
+            var response = await apiService.GetList<ProductRequest>(url, prefix, controller, Settings.TokenType, Settings.AccessToken);
+            var products = (List<ProductRequest>)response.Result;
+            foreach (var product in products.Where(p => p.Latitude != 0 && p.Longitude != 0).ToList())
+            {
+                var position = new Position(product.Latitude, product.Longitude);
+                pins.Add(new Pin
+                {
+                    Address = product.Remarks,
+                    Label = product.Description,
+                    Position = position,
+                    Type = PinType.Place,
+                });
+            }
+
+            return pins;
+        }
+
         private void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
         {
             var zoomLevel = double.Parse(e.NewValue.ToString()) * 18.0;
